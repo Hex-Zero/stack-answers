ce63e0be9537155b847d181d631899b820a52582
Ace Alrich <hex0@live.com>
2020-08-04 23:42:30 +0100

    finished addtional elements

--- Current/Main.js
+++ Current/Main.js
@@ -1,15 +1,25 @@
+const yourContent = document.createElement("div");
+yourContent.setAttribute("class", "content");
+const yourText = document.createTextNode("Lorem ipsum");
+yourContent.appendChild(yourText);
+
 const graphElem = document.getElementsByClassName("rows");
+const cssTemplateString = `td > .rows > .content{
+    position: absolute;
+    left: ${graphElem[0].clientWidth}px;
+    height: calc(${graphElem[0].clientHeight}px +2px);
+    width: calc(${graphElem[0].clientWidth}px + 2px);
+    background: white;
+    padding: 3rem;
+    border: 1px solid gray;
+    top: 0;
+}`;
+const styleTag = document.createElement("style");
+styleTag.innerHTML = cssTemplateString;
+document.head.insertAdjacentElement("beforeend", styleTag);
 
 for (let i = 0; i < graphElem.length; i++) {
-  const cssTemplateString = `td > .rows::before{
-        left: -100px;
-        top: calc(${graphElem[0].clientHeight}/2);
-    }`;
-  const styleTag = document.createElement("style");
-  styleTag.innerHTML = cssTemplateString;
-  document.head.insertAdjacentElement("beforeend", styleTag);
-
   graphElem[i].addEventListener("click", function (event) {
-    event.target.setAttribute("data-before", "anything");
+    event.target.appendChild(yourContent);
   });
 }
