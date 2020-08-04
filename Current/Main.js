1cb7024212176509790804643d207bd7d79bf5cc
Ace Alrich <hex0@live.com>
2020-08-04 21:16:02 +0100

    add before

--- Current/Main.js
+++ Current/Main.js
@@ -1,1 +1,15 @@
-$(document).ready(function (e) {});
+const graphElem = document.getElementsByClassName("rows");
+
+for (let i = 0; i < graphElem.length; i++) {
+  const cssTemplateString = `td > .rows::before{
+        left: -100px;
+        top: calc(${graphElem[0].clientHeight}/2);
+    }`;
+  const styleTag = document.createElement("style");
+  styleTag.innerHTML = cssTemplateString;
+  document.head.insertAdjacentElement("beforeend", styleTag);
+
+  graphElem[i].addEventListener("click", function (event) {
+    event.target.setAttribute("data-before", "anything");
+  });
+}
