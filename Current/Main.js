42e9877bee84ded925b591c1d358c43a6011e14c
Ace Alrich <hex0@live.com>
2020-08-08 00:07:40 +0100

    new

--- Current/Main.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-  var x = document.getElementById("nav-toggle");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
