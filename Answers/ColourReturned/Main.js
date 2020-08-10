62f98e0934023c4fb15a7de1c4444e25df49fb40
Ace Alrich <hex0@live.com>
2020-08-10 22:06:33 +0100

    new

---
+++ Answers/ColourReturned/Main.js
@@ -0,0 +1,5 @@
+console.log($("header").css("background-color"));
+
+const changingElement = $(".colour-return");
+
+console.log(changingElement.css("background-color"));
--- Current/Main.js
+++
@@ -1,5 +0,0 @@
-console.log($("header").css("background-color"));
-
-const changingElement = $(".colour-return");
-
-console.log(changingElement.css("background-color"));
