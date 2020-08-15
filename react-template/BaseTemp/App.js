af6661106473ab8cc6ee6cdfec25f5eeee9ee9ce
Ace Alrich <hex0@live.com>
2020-08-15 09:50:30 +0100

    react base

---
+++ react-template/BaseTemp/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <section></section>
+      <footer>
+        <span>Hex Zero</span>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
