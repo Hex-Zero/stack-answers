8e8ba0ab3936a705e8d6a3af05699c3d783231d2
Ace Alrich <hex0@live.com>
2020-09-21 23:16:29 +0100

    create button

--- Current/Main.js
+++ Current/Main.js
@@ -1,9 +1,23 @@
 const displayPressedButton = document.getElementById("display-presssed-button");
 
-const targetHtml = document.documentElement;
+const seciton = document.getElementById("section");
+const buttonBody = document.createElement("div");
+buttonBody.classList.add("button-body");
+const buttonText = document.createElement("div");
+buttonText.classList.add("display-presssed-button");
+buttonBody.appendChild(buttonText);
 
+seciton.appendChild(buttonBody);
+
+const changeText = buttonBody.getElementsByClassName(
+  "display-presssed-button"
+)[0];
+
+const targetHtml = document.documentElement;
 targetHtml.addEventListener("keydown", (e) => {
-  displayPressedButton.innerHTML = null;
-  displayPressedButton.innerHTML = e.key.toUpperCase();
-  console.log(e);
+  changeText.innerHTML = e.key.toUpperCase();
+  seciton.appendChild(buttonBody);
+  // displayPressedButton.innerHTML = null;
+  // displayPressedButton.innerHTML = e.key.toUpperCase();
+  // console.log(e);
 });
