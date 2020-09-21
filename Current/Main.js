edddbbc850fdabfdcdcb7e11e29f1163f6d3c9db
Ace Alrich <hex0@live.com>
2020-09-21 23:28:58 +0100

    prevent default

--- Current/Main.js
+++ Current/Main.js
@@ -1,23 +1,26 @@
 const displayPressedButton = document.getElementById("display-presssed-button");
-
 const seciton = document.getElementById("section");
-const buttonBody = document.createElement("div");
-buttonBody.classList.add("button-body");
-const buttonText = document.createElement("div");
-buttonText.classList.add("display-presssed-button");
-buttonBody.appendChild(buttonText);
 
-seciton.appendChild(buttonBody);
-
-const changeText = buttonBody.getElementsByClassName(
-  "display-presssed-button"
-)[0];
+function createButton(e) {
+  const buttonBody = document.createElement("div");
+  buttonBody.classList.add("button-body");
+  const buttonText = document.createElement("div");
+  buttonText.classList.add("display-presssed-button");
+  buttonText.innerHTML =
+    e.code === "Space" ? e.code.toUpperCase() : e.key.toUpperCase();
+  buttonBody.appendChild(buttonText);
+  seciton.appendChild(buttonBody);
+}
 
 const targetHtml = document.documentElement;
 targetHtml.addEventListener("keydown", (e) => {
-  changeText.innerHTML = e.key.toUpperCase();
-  seciton.appendChild(buttonBody);
-  // displayPressedButton.innerHTML = null;
-  // displayPressedButton.innerHTML = e.key.toUpperCase();
-  // console.log(e);
+  createButton(e);
+  e.preventDefault();
+});
+
+targetHtml.addEventListener("keyup", (e) => {
+  e.preventDefault();
+});
+targetHtml.addEventListener("keypress", (e) => {
+  e.preventDefault();
 });
